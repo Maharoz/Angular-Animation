4335c751bcf21abba3d00186e2b84865c0d93ebd
maharoz <mahruj66@gmail.com>
2019-02-24 22:31:15 +0600

    avance animation implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,17 +15,41 @@ import { trigger,state,style, transition, animate } from '@angular/animations';
         transform : 'translateX(100px)'
         
       })),
+      transition('normal <=> highlighted' , animate(300)),
+     // transition('normal => highlighted' , animate(800)),
+    ]),
+    trigger('wildState',[
+      state('normal',style({
+        'background-color' : 'red',
+        transform : 'translateX(0) scale(1)'
+      })),
+      state('highlighted',style({
+        'background-color' : 'blue',
+        transform : 'translateX(100px) scale(1)'
+        
+      })),
+      state('shrunken',style({
+        'background-color' : 'green',
+        transform : 'translateX(0px) scale(0.5)'
+        
+      })),
       transition('normal => highlighted' , animate(300)),
-      transition('highlighted => normal' , animate(800)),
+      transition('normal => highlighted' , animate(800)),
+      transition('shrunken <=> *' , animate(500)),
     ])
   ]
 })
 export class AppComponent {
   state='normal';
+  wildState='normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
+  onShrink(){
+    this.wildState = "shrunken"
+  }
   onAnimate(){
     this.state == 'normal' ? this.state ='highlighted' : this.state = 'normal';
+    this.wildState == 'normal' ? this.wildState ='highlighted' : this.wildState = 'normal';
   }
     onAdd(item) {
       this.list.push(item);
